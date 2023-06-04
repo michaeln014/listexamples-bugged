6cdf965b32a3a2a464fd87d82fbef55d06c13194
michaeln014 <michaelnguyen1475065@gmail.com>
2023-06-04 16:55:24 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -12,18 +12,18 @@ class ListExamples {
     List<String> result = new ArrayList<>();
     for (String s : list) {
       if (sc.checkString(s)) {
-        result.add(s);
+        result.add(1, s);
       }
     }
     return result;
   }
 
-
+  
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
-    int index1 = 0, index2 = 1;
+    int index1 = 0, index2 = 0;
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
         result.add(list1.get(index1));
