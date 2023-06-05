7c4468bf072577cc3138780a102e1ff35c1f456b
michaeln014 <michaelnguyen1475065@gmail.com>
2023-06-04 17:02:00 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -12,7 +12,7 @@ class ListExamples {
     List<String> result = new ArrayList<>();
     for (String s : list) {
       if (sc.checkString(s)) {
-        result.add(1, s);
+        result.add(s);
       }
     }
     return result;
@@ -24,7 +24,7 @@ class ListExamples {
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
-    while(index1 < list1.size() && index2 < list2.size()) {
+    while(index1 <= list1.size() && index2 <= list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
         result.add(list1.get(index1));
         index1 += 1;
@@ -44,6 +44,5 @@ class ListExamples {
     }
     return result;
   }
-
-
+  
 }
